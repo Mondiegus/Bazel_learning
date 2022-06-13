953e170bf16d6ef159f32ba7919b76fe45a366a6
maciej.jarosz <maciej.jarosz@haptology.com>
2022-06-13 19:59:47 +0200

    first commit

---
+++ Ch2/source/tests/tests.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <Api.h>
+#include <iostream>
+#include <HapticApiWrapper.h>
+
+using namespace Haptology;
+
+TEST(ch2, assert_true)
+{
+    ASSERT_EQ(2, 2);
+}
+
+TEST(ch2, assert_false)
+{
+    ASSERT_EQ(2, 2);
+}
