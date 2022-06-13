d4a137c6f6bf4fd738a7b5aa86b8194e40b5710f
maciej.jarosz <maciej.jarosz@haptology.com>
2022-06-13 22:44:06 +0200

    CI/cd

--- Ch2/tests/tests.cpp
+++ Ch2/tests/tests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
-
+#include <01.h>
+#include <02.h>
 
 TEST(ch2, assert_true)
 {
@@ -8,5 +9,47 @@ TEST(ch2, assert_true)
 
 TEST(ch2, assert_false)
 {
-    ASSERT_EQ(2, 2);
+    ASSERT_NE(2, 3);
+}
+
+TEST(ch2, t01_passed_defConstructor)
+{
+    Citizen citizen;
+    ASSERT_EQ(citizen.getSurname(), "Kowalski");
+    ASSERT_EQ(citizen.getAddress(), "nope");
+}
+
+TEST(ch2, t01_passed_customConstructor)
+{
+    Citizen citizen("aa", "bb");
+    ASSERT_EQ(citizen.getSurname(), "aa");
+    ASSERT_EQ(citizen.getAddress(), "bb");
+}
+
+TEST(ch2, t01_failed)
+{
+    Citizen citizen("aa", "bb");
+    ASSERT_NE(citizen.getSurname(), "Kowalski");
+    ASSERT_NE(citizen.getAddress(), "nope");
+}
+
+TEST(ch2, t02_passed_00)
+{
+    MilesToMetersConverter converter(1.f);
+    double value = converter.getValueInMeters();
+    ASSERT_EQ(value, 1852);
+}
+
+TEST(ch2, t02_passed_01)
+{
+    MilesToMetersConverter converter(0.1f);
+    double value = converter.getValueInMeters();
+    ASSERT_EQ(float(value), float(185.2)); // gtest have problems with double
+}
+
+TEST(ch2, t02_failed)
+{
+    MilesToMetersConverter converter(0.1);
+    int value = converter.getValueInMeters();
+    ASSERT_NE(value, 185.2);
 }
