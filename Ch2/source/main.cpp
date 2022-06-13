efa02d801a2b059a1b15218b46f8b64ac562e0a0
Mondiegus <Mondiegus9@gmail.com>
2022-06-13 23:08:15 +0200

    01_03 (#3)
    Co-authored-by: maciej.jarosz <maciej.jarosz@haptology.com>

--- Ch2/source/main.cpp
+++ Ch2/source/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <01.h>
 #include <02.h>
+#include <03.h>
 
 using namespace std;
 
@@ -17,9 +18,15 @@ void t02()
     cout << value << endl;
 }
 
+void t03()
+{
+    Poem poem;
+}
+
 int main()
 {
     t01();
     t02();
+    t03();
     return 0;
 }
